d4c68ac5c58655d9f1fe076ee5123ce6a81a0f56
a26232933 <a094868@gmail.com>
2020-07-12 15:33:41 +0800

    CDN添加

---
+++ js/main.js
@@ -0,0 +1,17 @@
+AOS.init();
+
+$('#banner').slick({
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false
+});
+$('#brand').slick({
+    
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots: true,
+    arrows: false,
+    
+});
