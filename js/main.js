3ce4c0f9d2629f1f8d44230920ffdf8c255ed919
a26232933 <a094868@gmail.com>
2020-07-26 16:57:15 +0800

    第一頁完成

--- js/main.js
+++ js/main.js
@@ -1,16 +1,8 @@
 AOS.init();
 
-$('#banner').slick({
-    autoplay: true,
-    autoplaySpeed: 1000,
-    arrows: false
-});
 $('#brand').slick({
-    slidesToShow: 5,
-    slidesToScroll: 2,
     autoplay: true,
-    autoplaySpeed: 1000,
-    dots: true,
+    autoplaySpeed: 5000,
     arrows: false,
-    
+    dots: true,
 });
